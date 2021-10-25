49b2a68c833f87508845e9045393fc33093ec8d0
deepaksilawat <deepak.silawat@ibekinc.com>
2021-10-25 21:08:58 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoDB = require('./config/db');
+
+const app = express();
+mongoDB();
+
+app.use(express.json({extended: false}));
+
+app.get('/', (req, res) =>{
+    res.send('API is Running');
+})
+
+// Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>{
+    console.log(`Server is running port ${PORT}`);
+})
